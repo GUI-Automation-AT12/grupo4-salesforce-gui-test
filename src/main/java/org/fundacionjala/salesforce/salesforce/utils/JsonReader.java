ee6cc10ca2ab947937a5228d0e30127c01859c18
roht0515 <rodrigo051599@gmail.com>
2021-01-16 12:14:09 -0400

    json reader

---
+++ src/main/java/org/fundacionjala/salesforce/salesforce/utils/JsonReader.java
@@ -0,0 +1,35 @@
+package org.fundacionjala.salesforce.salesforce.utils;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonReader {
+
+    private static String filePath;
+    public JsonReader(final String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Try to read a json file and return a JSONArray with the read info.
+     * @return JSONArray;
+     */
+    public static JSONArray jsonArrayFromJsonFile() throws ParseException {
+        try {
+            JSONParser jsonParser = new JSONParser();
+            FileReader reader = new FileReader(filePath);
+            Object obj = jsonParser.parse(reader);
+            return (JSONArray) obj;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JSONArray();
+    }
+}
---
+++ src/main/java/org/fundacionjala/salesforce/salesforce/utils/JsonUser.java
@@ -0,0 +1,99 @@
+package org.fundacionjala.salesforce.salesforce.utils;
+
+import org.json.simple.parser.ParseException;
+
+import java.util.Map;
+
+public class JsonUser extends JsonReader {
+
+    private static String jsonPath = "src/main/java/trello/config/json/User.json";
+    private static JsonUser environment;
+    private Map<String, String> jsonUser;
+
+    /**
+     * Json User constructor.
+     */
+    private JsonUser() {
+        super(jsonPath);
+        jsonUser = convertToMap();
+    }
+
+    /**
+     * This method returns the only one instance of JsonUser.
+     * @return
+     */
+    public static JsonUser getInstance() {
+        if (environment == null) {
+            environment = new JsonUser();
+        }
+        return environment;
+    }
+
+    /**
+     * This method converts the Json to a map.
+     * @return
+     */
+    private Map<String, String> convertToMap() {
+        try {
+            return (Map<String,String>) JsonReader.jsonArrayFromJsonFile().get(0);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * This method returns the name.
+     * @return
+     */
+    public String getName() {
+       return jsonUser.get("name");
+    }
+
+    /**
+     * This method returns the email.
+     * @return
+     */
+    public String getEmail() {
+        return jsonUser.get("email");
+    }
+
+    /**
+     * This method returns the password.
+     * @return
+     */
+    public String getPassword() {
+        return jsonUser.get("password");
+    }
+
+    /**
+     * This method returns the username.
+     * @return
+     */
+    public String getUserName() {
+        return jsonUser.get("userName");
+    }
+
+    /**
+     * This method returns the bibliography.
+     * @return
+     */
+    public String getBibliography() {
+        return jsonUser.get("bibliography");
+    }
+
+    /**
+     * This method returns the token.
+     * @return
+     */
+    public String getToken() {
+        return jsonUser.get("token");
+    }
+
+    /**
+     * This method returns the key.
+     * @return
+     */
+    public String getKey() {
+        return jsonUser.get("key");
+    }
+}
